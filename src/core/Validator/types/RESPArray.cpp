ba898fd3756c741d9a16c2c076d7c79099a05784
Anmol-user <singhanmol0984@gmail.com>
2025-01-15 21:27:08 +0530

    implement vaildation for arrays

--- src/core/Validator/types/RESPArray.cpp
+++ src/core/Validator/types/RESPArray.cpp
@@ -3,6 +3,37 @@ using namespace std;
 
 bool RESPArray::validate(const string& data, size_t& ptr){
 
+    string numElements = "";
+
+    while(ptr < data.size() && isdigit(data[ptr])){
+        numElements += data[ptr++];
+    }
+
+    if(numElements.empty()){
+        return false;
+    }
+
+    if(!RESPFactory::consumeCRLF(data, ptr)){
+        return false;
+    }
+
+    int numberOfElements = stoi(numElements);
+
+    for(int i = 0;i < numberOfElements; i++){
+        if(ptr >= data.size()){
+            return false;
+        }
+        
+        char prefix = data[ptr++];
+        RESPTypes elementType = RESPFactory::determineRESPType(prefix);
+        unique_ptr<RESPElement> respElement = RESPFactory::createRESPElement(elementType);
+        
+        if(respElement->validate(data, ptr)){
+            return false;
+        }
+
+    }
+
 
     return true;
 }
